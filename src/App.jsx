99c5ae0b90389614432146d8527f6186f63f139a
Arno Siegenthaler <arno@pop-os-pc.oberon>
2022-05-05 22:15:03 +0200

    update react

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import MovingGibbix from './MovingGibbix';
+import GibbixJump from './GibbixJump';
+
+import top from './img/top_edit.jpg';
+import infra from './img/infra.jpg';
+import audio from './audio/kekw.ogg';
+import AudioPlayer from 'react-h5-audio-player';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gibbixes: [],
+      showJump: false,
+      infraX: 0,
+      infraStatus: true
+    };
+    console.log("KEKW")
+
+    this.move = this.move.bind(this);
+  }
+
+  move() {
+    if (this.state.infraX > document.documentElement.offsetWidth - this.infra.clientWidth) {
+      this.setState({ infraStatus: false });
+    }
+    if (this.state.infraX <= 0) {
+      this.setState({ infraStatus: true });
+    }
+
+    this.setState({
+      infraX: this.state.infraStatus ? this.state.infraX + 5 : this.state.infraX - 5
+    })
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.gibbixes.length >= 10) {
+        return;
+      }
+      this.setState({
+        gibbixes: [...this.state.gibbixes, <MovingGibbix key={Date.now()} />]
+      });
+    }, 1000);
+
+    setInterval(this.move, 1);
+  }
+
+  onPlay() {
+    console.log('Started playing');
+    setTimeout(() => {
+      this.setState({ showJump: true });
+      setTimeout(() => {
+        this.setState({ showJump: false });
+      }, 2000);
+    }, 56000);
+    //}, 2000);
+  }
+  playAudio(){
+    new Audio(audio).play();
+  }
+
+  render() {
+    return (
+    <div>
+        <div
+          className="full-bg"
+          style={{
+            backgroundImage: 'url(' + top + ')',
+            display: this.state.showJump ? 'none' : 'block'
+          }}
+          
+        >
+          {this.state.gibbixes}
+          <img style={{
+            position: 'absolute',
+            left: this.state.infraX,
+            zIndex: 10
+          }} src={infra} alt="infrasturcture" ref={elem => { this.infra = elem; }} />
+          <div className="hidden-player">
+          
+           <AudioPlayer style={{zIndex: 20}}
+            src={audio}
+            autoPlay={true}
+            loop={true}
+            showSkipControls={false}
+            showJumpControls={false}
+           />
+           
+          </div>
+        </div>
+        {this.state.showJump && <GibbixJump />}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MovingGibbix.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import smartlearnAlpha from './img/smartlearn_alpha.png';
+
+class MovingGibbix extends React.Component {
+  performGibbixMove() {
+
+    const getRandom = max => {
+      return Math.floor(Math.random() * max) - max / 2;
+    };
+
+    this.leftSpeed = this.leftSpeed + getRandom(100);
+    this.topSpeed = this.topSpeed + getRandom(100);
+
+    this.gibbix.style.top = this.gibbix.offsetTop + getRandom(50) + 'px';
+    this.gibbix.style.left = this.gibbix.offsetLeft + getRandom(50) + 'px';
+
+    this.rotation = this.rotation + getRandom(50);
+    this.width = this.width + getRandom(50);
+
+    if (this.width < 50) {
+      this.width = this.width = 50;
+    }
+
+    this.gibbix.style.transform = 'rotate(' + this.rotation + 'deg)';
+    this.gibbix.style.width = this.width + 'px';
+  }
+
+  componentDidMount() {
+    this.gibbix.style.top = Math.floor(window.innerHeight / 2) - 200 + 'px';
+    this.gibbix.style.left = Math.floor(window.innerWidth / 2) - 300 + 'px';
+
+    setInterval(this.performGibbixMove, 1);
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.performGibbixMove = this.performGibbixMove.bind(this);
+
+    this.leftSpeed = 0;
+    this.topSpeed = 0;
+    this.rotation = 0;
+    this.width = 600;
+  }
+
+  render() {
+    return (
+      <img
+        src={smartlearnAlpha}
+        style={{
+          position: 'absolute',
+          transition: 'all 0.1s',
+          zIndex: 50
+        }}
+        ref={gibbix => {
+          this.gibbix = gibbix;
+        }}
+        alt="GibbiX"
+      />
+    );
+  }
+}
+
+export default MovingGibbix;
